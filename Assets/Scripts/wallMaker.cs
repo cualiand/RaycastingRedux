cf6296e132712e7b67c7934197d096e6a84dbc33
Andrew Cualing <ayc300@nyu.edu>
2016-04-05 07:18:00 -0400

    feature complete- maze gen complete, needs art pass

--- Assets/Scripts/wallMaker.cs
+++ Assets/Scripts/wallMaker.cs
@@ -6,10 +6,13 @@ public class wallMaker : MonoBehaviour {
     public Transform wallPiece;
     private int counter = 0;
     public int counterNumber = 25;
+    public Transform pathmakerPrefab;
+    public Transform centerPiece;
+    public Vector3 startingPoint;
 
 	// Use this for initialization
 	void Start () {
-	
+        startingPoint = transform.position;
 	}
 	
 	// Update is called once per frame
@@ -17,16 +20,27 @@ public class wallMaker : MonoBehaviour {
         if (counter < counterNumber)
         {
             float randNum = Random.Range(0f, 1f);
-            if (randNum < .25f)
+
+            if ((transform.position - centerPiece.position).magnitude >= 18f)
+            {
+                Debug.Log("Flipped");
+                float randNum2 = Random.Range(0f, 1f);
+                transform.position = startingPoint + new Vector3(Random.Range(-10f, 10f), 0f, Random.Range(-10f, 10f));
+            }
+            if (randNum < .1f)
             {
                 transform.Rotate(0f, 90f, 0f);
             }
-            else if (randNum > .25f && randNum < .5f)
+            else if (randNum > .1f && randNum < .2f)
             {
                 transform.Rotate(0f, -90f, 0f);
             }
-            Instantiate(wallPiece, transform.position, Quaternion.identity);
-            transform.Translate(5f, 0f, 0f);
+            if (randNum > 1f)
+            {
+                Instantiate(pathmakerPrefab, transform.position, Quaternion.identity);
+            }
+            Instantiate(wallPiece, transform.position, transform.rotation);
+            transform.Translate(1.25f, 0f, 0f);
             counter++;
 
         }
---
+++ Assets/wallMaker2.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wallMaker2 : MonoBehaviour {
+
+
+    public Transform wallPiece;
+  //  private int counter = 0;
+    //public int counterNumber = 25;
+    //public Transform pathmakerPrefab;
+    //public Transform centerPiece;
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < 10; i++)
+        {
+            float randNum = Random.Range(0f, 1f);
+            if (randNum < .5f)
+            {
+                Instantiate(wallPiece, transform.position, Quaternion.identity);
+            }
+            transform.Translate(0f, 0f, 3.22f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
